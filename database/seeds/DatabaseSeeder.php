b02b7a762049f7fe823306d1d3fd285a904a011a
floLfg <florian.laforgue@gmail.com>
2017-12-29 17:20:27 +0100

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+        	'name' => 'Florian',
+        	'email' => 'florian.laforgue@gmail.com',
+        	'password' => bcrypt('azerty')
+        ]);
     }
 }
